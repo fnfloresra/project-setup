26848f965264d4094a67cc43fe92064adb94a276
fnfloresra <fnfloresr@outlook.com>
2026-06-29 09:28:42 -0500

    testing project 6 setup commit

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print ("Hello World")
+# This is a sample Python script that prints "Hello World" five times
+
+if __name__ == "__main__":
+    # This block will only execute if the script is run directly, not imported as a module.
+    print("Script executed directly.")
+variable = 5
+if variable == 10:
+    print(f"The variable is set to {variable}.")
+    for i in range(5):
+        print("Hello World")
+else:
+    print("The variable is not set.")
